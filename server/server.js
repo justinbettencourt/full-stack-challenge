3953f695627177ad13eb38d06e71ac9ac6f28b0f
Justin Bettencourt <justinvbettencourt@gmail.com>
2019-02-22 15:43:08 -0500

    Mock Server Working
    The mock server seems to be working, but whenever I try to do a push to the location in the documentation, I'm getting some errors that it can't be reached. Will need to look into this further.
    
    I have 2 JSON files to read data from:
    - ZomaronAPI.json: Offline version of the API to test
    - UserData.json: Where I will be storing a copy of the information input by the user.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
-const axios = require('axios')
-var config = require('./ZomaronAPI.json');
-
+const fs = require('fs');
+var zomaronAPI = require('./ZomaronAPI.json');
+var userData = require('./UserData.json');
+var newUser = './UserData.json'
 var cors = require('cors')
 
 // Was getting a CORS issue referencing the server from the Vue.js app
 app.use(cors());
+app.use(express.json());
 
 app.get('/', function (req, res) {
-	res.send(config)
+	res.send(zomaronAPI)
 })
 
-app.post('/', function (req, res) {
-	res.send(config)
+app.get('/userData', function (req, res) {
+	res.send(userData)
+})
+
+app.post('/userData', function (req, res) {	
+	console.log("Got response: " + res.statusCode);
+	console.log(req.body);
+	res.send(req.body);
+	fs.createWriteStream(newUser).write(JSON.stringify(req.body));
 })
 
 app.listen(3000, function () {
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,100 @@
+<template>
+  	<div class="hello">
+		<!-- Name Text Box -->
+		<input v-model="name" type="text" placeholder="Name">
+		<br><br>
+		<!-- isTest Checkbox to determine if information is valid -->
+		<div>
+			<input type="checkbox" id="checkbox" v-model="isTest">
+			<label for="checkbox">isTest ({{ isTest }})</label>
+		</div>
+		<!-- Begin looping through the JSON date we received and stored in the data object "Questions" -->
+		<ul>
+			<li v-for="(question, index) in Questions" :key="index.id">
+				<div>
+					<p>Question #{{ question.number }}</p>
+					<p>{{ question.description }}</p>
+					<textarea rows="4" cols="50" v-model="question.answer"></textarea>
+				</div>
+			</li>
+		</ul>
+		<button @click="postToServer()">Send</button>
+  	</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+	name: "HelloWorld",
+	data() {
+		return {
+			name: '',
+			isTest: true,
+			Questions: {},
+		}
+	},
+	methods: {
+		postToServer() {
+			// We are going to set the data objects as variables to pass through the POST.
+			var name = this.name
+			var isTest = this.isTest
+			var questions = this.Questions
+			
+			// When ready to submit, POST the data to the web server. Data will be displayed at /userData
+			axios.post('http://localhost:3000/userData', { 
+				name,
+				isTest,
+				questions
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json'	
+				}
+			})
+			.then((response) => {
+				// response.data = newUser;
+				console.log('Sending to POST: ')
+				console.log(response.data)
+			})
+			
+		}
+	},
+  	mounted() {
+		//axios.get('https://api.zomaron.com/v1/coding/test1')
+		// Using the API locally (on the web server through express.js) at the moment to get things working first.
+		axios.get("http://localhost:3000/", {
+			headers: {
+				'Content-Type': 'application/json'	
+			}
+		})
+		.then(response => {
+			// console.log(response.data);
+			this.Questions = response.data;
+			console.log('GET API data from web server: ')
+			console.log(this.Questions);
+		})
+		.catch(error => {
+			console.log(error);
+		});
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
